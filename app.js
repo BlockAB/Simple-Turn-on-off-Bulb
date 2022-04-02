2963fcd88e05f48d765782be2e6a33e108afa89f
BlockAB <101047978+BlockAB@users.noreply.github.com>
2022-04-02 10:57:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+
+
+const bulbSwitch = document.querySelector('#bulbSwitch');
+const bulb = document.querySelector('#bulb');
+
+bulbSwitch.addEventListener('click', function(){
+  if (bulb.src.match("off")) {
+    bulb.src = "./bulb-on.gif";  
+     bulbSwitch.innerHTML = 'Turn off';
+  }
+  
+  else{
+    bulb.src = "./bulb-off.gif";
+    bulbSwitch.innerHTML = 'Turn on';
+  }
+  
+})
